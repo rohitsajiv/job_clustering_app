fcb70490b37c69703a52c28ae6bbc0888844a320
Rohit Sajiv <rohitsajiv11@gmail.com>
2025-10-21 20:43:32 +0530

    Code updation

--- cluster.py
+++ cluster.py
@@ -18,7 +18,7 @@ def load_model():
 def classify_jobs(jobs_df, tfidf, model):
     # Ensure column name matches exactly: 'Skills'
     skills = jobs_df['Skills'].tolist()
-    X_new = tfidf.transform(skills)
+    X_new = tfidf.fit_transform(skills)
     clusters = model.predict(X_new)
     jobs_df['cluster'] = clusters
     return jobs_df
